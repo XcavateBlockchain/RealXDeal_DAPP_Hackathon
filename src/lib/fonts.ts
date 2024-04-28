17edc94cf29e4473cfcea276c6af799bd5ee6026
izaakwalz <chukwumaizaak@gmail.com>
2024-04-28 19:57:19 +0100

    frat: add default fonts to layout

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,17 +1,17 @@
-import localFont from "next/font/local";
+import localFont from 'next/font/local';
 
-export const fontText = localFont({
+export const fontHeading = localFont({
   src: [
     {
-      path: "/fonts/DrukTextWideMediumTrial.otf",
-      weight: "500",
-      style: "normal",
+      path: '../assets/fonts/DrukTextWideMediumTrial.otf',
+      weight: '500',
+      style: 'normal'
     },
     {
-      path: "/fonts/DrukTextWideBoldTrial.otf",
-      weight: "700",
-      style: "normal",
-    },
+      path: '../assets/fonts/DrukTextWideBoldTrial.otf',
+      weight: '700',
+      style: 'normal'
+    }
   ],
-  variable: "--font-text",
+  variable: '--font-heading'
 });
